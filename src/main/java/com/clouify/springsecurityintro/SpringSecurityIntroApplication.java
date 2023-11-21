2c27ea4198f2865fd036bd8275a197e21141f8cd
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2023-11-21 03:55:15 +0300

    initial commit

---
+++ src/main/java/com/clouify/springsecurityintro/SpringSecurityIntroApplication.java
@@ -0,0 +1,13 @@
+package com.clouify.springsecurityintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityIntroApplication.class, args);
+    }
+
+}
